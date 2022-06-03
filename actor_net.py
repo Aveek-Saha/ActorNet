25c8d0f76ca715d2eaf025a0fe0872d25ba27820
Aveek Saha <aveek.s98@gmail.com>
2022-06-03 20:56:54 +0530

    Get actor id

--- actor_net.py
+++ actor_net.py
@@ -1,3 +1,27 @@
+import urllib
+import urllib.request
+import urllib.parse
+from unidecode import unidecode
+
+import json
+
 import config
 
+TMDB_ACTOR_URL = "https://api.themoviedb.org/3/search/person?api_key=%s&language=en-US&query=%s&page=1"
+
 tmdb_api_key = config.tmdb_api_key
+
+def get_actor_id(name):
+
+    url = TMDB_ACTOR_URL % (tmdb_api_key, urllib.parse.quote(name))
+    response = urllib.request.urlopen(url)
+    res_data = response.read()
+    jres = json.loads(res_data)
+
+    if jres['total_results'] > 0:
+        actor = jres['results'][0]
+        return actor['id']
+    else:
+        return {}
+
+print(get_actor_id("Tom Holland"))
