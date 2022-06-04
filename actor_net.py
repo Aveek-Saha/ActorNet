c7230e5ff7aa395c235ad2b30f14e0758f060c34
Aveek Saha <aveek.s98@gmail.com>
2022-06-04 21:05:10 +0530

    Get actor credits

--- actor_net.py
+++ actor_net.py
@@ -8,6 +8,7 @@ import json
 import config
 
 TMDB_ACTOR_URL = "https://api.themoviedb.org/3/search/person?api_key=%s&language=en-US&query=%s&page=1"
+TMDB_CREDITS_URL = "https://api.themoviedb.org/3/person/%s/combined_credits?api_key=%s&language=en-US"
 
 tmdb_api_key = config.tmdb_api_key
 
@@ -22,6 +23,20 @@ def get_actor_id(name):
         actor = jres['results'][0]
         return actor['id']
     else:
-        return {}
+        return -1
 
-print(get_actor_id("Tom Holland"))
+def get_actor_credits(actor_id):
+
+    url = TMDB_CREDITS_URL % (urllib.parse.quote(str(actor_id)), tmdb_api_key)
+    response = urllib.request.urlopen(url)
+    res_data = response.read()
+    jres = json.loads(res_data)
+
+    return jres['cast']
+
+actor_name = "Tom Holland"
+actor_id = get_actor_id(actor_name)
+
+actor_credits = get_actor_credits(actor_id)
+
+print(actor_credits)
