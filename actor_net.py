cf7746ea8e9176fd648ed0d7d6d1036e588348f4
Aveek Saha <aveek.s98@gmail.com>
2022-06-10 01:18:02 +0530

    Collect second order credits

--- actor_net.py
+++ actor_net.py
@@ -42,7 +42,8 @@ def get_actor_id(name):
 
 def get_actor_credits(actor_id):
 
-    url = TMDB_ACTOR_CREDITS_URL % (urllib.parse.quote(str(actor_id)), tmdb_api_key)
+    url = TMDB_ACTOR_CREDITS_URL % (
+        urllib.parse.quote(str(actor_id)), tmdb_api_key)
     response = urllib.request.urlopen(url)
     res_data = response.read()
     jres = json.loads(res_data)
@@ -51,11 +52,13 @@ def get_actor_credits(actor_id):
     credit_ids = []
 
     for credit in credits:
-        json.dump(credit, open("{}/{}.json".format(MOVIE_DIR, str(credit['id'])), "w"))
+        json.dump(credit, open(
+            "{}/{}.json".format(MOVIE_DIR, str(credit['id'])), "w"))
         credit_ids.append(str(credit['id']))
-    
+
     with open('{}/{}.txt'.format(DATA_DIR, actor_name), 'w', encoding='utf-8') as f:
         f.write(str.join('\n', (str(x) for x in credit_ids)))
+
 
 def get_ego_center_credits(actor_name):
     # create the users friend list
@@ -69,10 +72,13 @@ def get_ego_center_credits(actor_name):
         pass
     return (credit_ids)
 
+
 def get_movie_actors(credit_ids):
+    all_actors = []
     for credit in tqdm(credit_ids):
         if is_folder_exists('{}/{}.json'.format(MOVIE_DIR, str(credit))):
-            saved = json.load(open("{}/{}.json".format(MOVIE_DIR, str(credit)), "r"))
+            saved = json.load(
+                open("{}/{}.json".format(MOVIE_DIR, str(credit)), "r"))
             if saved['media_type'] == 'movie':
                 url = TMDB_MOVIE_CREDITS_URL % (credit, tmdb_api_key)
             elif saved['media_type'] == 'tv':
@@ -87,28 +93,68 @@ def get_movie_actors(credit_ids):
 
                 for actor in actors:
                     actor_ids.append(str(actor['id']))
-                
-                with open('{}/{}.txt'.format(ACTOR_DIR, str(actor['id'])), 'w', encoding='utf-8') as f:
-                    f.write(str.join('\n', (str(x) for x in actor_ids)))
+                all_actors += actor_ids
+
+                # with open('{}/{}.txt'.format(ACTOR_DIR, str(actor['id'])), 'w', encoding='utf-8') as f:
+                #     f.write(str.join('\n', (str(x) for x in actor_ids)))
             except Exception as e:
                 print("Error collecting ", credit)
 
+    with open('{}/{}.txt'.format(DATA_DIR, "actor_ids"), 'w', encoding='utf-8') as f:
+        f.write(str.join('\n', (str(x) for x in list(set(all_actors)))))
 
+def get_actor_ids():
+    actor_ids = []
+    try:
+        with open('{}/{}.txt'.format(DATA_DIR, "actor_ids")) as f:
+            for line in f:
+                actor_ids.append(int(line))
+    except Exception as e:
+        print("Error: ", e)
+        pass
+    return actor_ids
+
+def get_second_order_credits(actor_ids):
+    for actor_id in tqdm(actor_ids):
+        try:
+            url = TMDB_ACTOR_CREDITS_URL % (
+                urllib.parse.quote(str(actor_id)), tmdb_api_key)
+            response = urllib.request.urlopen(url)
+            res_data = response.read()
+            jres = json.loads(res_data)
+
+            credits = jres['cast']
+            credit_ids = []
+
+            for credit in credits:
+                credit_ids.append(str(credit['id']))
+
+            with open('{}/{}.txt'.format(ACTORS_DIR, actor_id), 'w', encoding='utf-8') as f:
+                f.write(str.join('\n', (str(x) for x in credit_ids)))
+
+        except Exception as e:
+                print("Error: ", e)
 
 actor_name = "Tom Holland"
 DATA_DIR = "data"
 MOVIE_DIR = "{}/{}".format(DATA_DIR, "movies")
 ACTOR_DIR = "{}/{}".format(DATA_DIR, actor_name)
+ACTORS_DIR = "{}/{}".format(DATA_DIR, "actors")
 
 create_dir(MOVIE_DIR)
 create_dir(ACTOR_DIR)
+create_dir(ACTORS_DIR)
 
 # actor_id = get_actor_id(actor_name)
 
 # get_actor_credits(actor_id)
 
-credit_ids = get_ego_center_credits(actor_name)
+# credit_ids = get_ego_center_credits(actor_name)
 
-get_movie_actors(credit_ids)
+# get_movie_actors(credit_ids)
+
+actor_ids = get_actor_ids()
+
+get_second_order_credits(actor_ids)
 
 # print(actor_credits)
